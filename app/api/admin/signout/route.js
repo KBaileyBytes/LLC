40cd96675ba963a49ffbc37179b43b3a3fec8ce7
KBaileyBytes <kylebailey336@gmail.com>
2025-07-22 21:41:15 -0500

    updated sign-out route

--- app/api/admin/signout/route.js
+++ app/api/admin/signout/route.js
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const response = NextResponse.redirect(
-    new URL(
-      "/signIn",
-      process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
-    )
+    new URL("/signIn", "https://llc-drab.vercel.app/signIn")
   );
 
   response.cookies.set("admin-auth", "", {
