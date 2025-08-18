fe328db5d2945ac1f509651f2c2b50bd2b2b3ee7
KBaileyBytes <kylebailey336@gmail.com>
2025-08-17 19:22:15 -0500

    About updated with picture

--- app/about/page.js
+++ app/about/page.js
@@ -1,23 +1,34 @@
+import LLCBooth from "@/public/images/LLC_booth.jpg";
+import Image from "next/image";
+
 export default function AboutPage() {
   return (
-    <section className="p-16 my-8 ml-12 grid gap-4">
+    <section className="sm:p-16 my-8 ml-12 grid gap-4">
       <p className="koh-santepheap text-neutral-800 text-6xl font-bold py-4">
         About
       </p>
-      <p className="text-lg text-neutral-500 italic pb-4">
-        From canvas to coaster, each piece is thoughtfully crafted to bring
-        color, warmth, and joy into your space.
-      </p>
-      <section className="max-w-5xl grid gap-4 text-lg xl:text-xl">
-        <p>
-          Every piece I create is a celebration of color, nature, and
-          craftsmanship. Using resin as my canvas, I pour, shape, and design
-          each bowl, tray, and coaster with care, blending vibrant pigments and
-          textures to create something truly special for your home. Whether it’s
-          a beautiful resin turtle family or a abalone shell tray, my goal is to
-          craft items that bring a sense of joy and wonder into your everyday
-          life.
-        </p>
+      <section className="flex flex-col xl:flex-row gap-12 items-center xl:items-start">
+        <section>
+          <p className="text-lg text-neutral-500 italic pb-4">
+            From canvas to coaster, each piece is thoughtfully crafted to bring
+            color, warmth, and joy into your space.
+          </p>
+          <section className="text-lg xl:text-xl">
+            <p>
+              Hi I’m Leah, the artist behind Leelees Creationz. My passion for
+              resin art started with a simple curiosity and turned into a love
+              creating pieces with resin. I draw inspiration from my love of
+              colour and texture. From abalone shells to sparkle, each creation
+              is handcrafted with love. Thank you for visiting my website and
+              supporting my small business. This is truly a dream come true.
+            </p>
+          </section>
+        </section>
+        <Image
+          src={LLCBooth}
+          alt="LLC Booth"
+          className="rounded-lg max-w-3xl object-contain hidden lg:block"
+        />
       </section>
     </section>
   );
